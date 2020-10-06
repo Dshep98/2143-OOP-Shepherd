7c2fd00804ca8669288b8e0342e10bdc2361e812
Dominique N. Shepherd <dshep98@gmail.com>
2020-10-05 21:12:40 -0500

    updated comments

--- Assignments/A06/get_factors.cpp
+++ Assignments/A06/get_factors.cpp
@@ -1,43 +1,55 @@
-nclude <iostream>
+#include <iostream>
 #include<fstream> 
 using namespace std;
-  
+//Function: Prime number function
+//input: it takes in a value to determine if its prime
+//output: returns true for prime, false for not prime
+bool prime(int num)
+{
+  int i;
+  for(i=2; i< num; i++)
+  {
+    if(num % i==0)
+    {
+      return false;
+    } 
+  }
+  return true;
+}  
 
 
 int main()
 { //How to determine if a number is prime or not
-//with prime initally set as true.
-  int num; int i; bool prime=true;int count=1;
+  int num; int i;int count=1;bool result;
   //The lines declare input and output of the file.
-		ifstream infile;
-		ofstream outfile;
-		infile.open("num.txt");
-		outfile.open("output.txt");
-		outfile << "Name:Dominique Shepherd\n\n";
- 
-	//reads until the end of the file
+	ifstream infile;
+	ofstream outfile;
+	infile.open("num.txt");
+	outfile.open("output.txt");
+	outfile << "Name:Dominique Shepherd\n\n";
+ //Reads in values from a file until the end of a line
   while(infile >> num)
-  {  
-    //prints the number its on and its factors
-    outfile << "Number "<< count << ": " << num << "-Factors: ";
-    //the loop here starts at 2 since 1 is neither a prime or composite and from there it keeps
-    //going until its either determined as prime or not
-    for(i=2; i<= num/2; i++)
+  {  //result takes in the first integer to see if its prime or not.
+    result=prime(num);
+    if (result==true)// if result returns true it prints prime.
     {
-      if(num % i==0)// if the remainder is 0 prime becomes false and it prints out all the factors of a number
-      {//Since a prime number is divisible by its self and 1.
-        prime=false;
-        outfile << " " <<i << ",";
-      } 
+      outfile << "Number "<< count << ": "<< "prime\n";
     }
-    outfile << '\n';
-    if (prime)// this operates if prime stays true 
+    if(result==false)//if result returns false it prints the factors
     {
-      outfile << "Prime\n";
+      outfile << "Number "<< count << ": " << num << "-Factors: ";
+      for(i=2; i<= num/2; i++)//the loop starts at 2 since 1 is neither prime nor composite
+      {
+        if(num % i==0)//this runs if remainder of num divided by i equals zero.
+        {
+          outfile<< " " << i<< ",";// prints out the factors of number being read from file
+        } 
+      }
+      outfile << "\n";
     }
-      
-    count++;//updates the counter to know what number were in
+    count++;//this just updates the number its on in the file
  }
 
   return 0;
 }
+
